64213f215821ab86108ae3eff0c64d7290728828
Shivom@1 <pandeyshivom43@gmail.com>
2020-01-11 02:18:53 -0800

    some change in signIn xml

---
+++ app/src/main/java/com/shivom/foodit/Model/User.java
@@ -0,0 +1,29 @@
+package com.shivom.foodit.Model;
+
+public class User {
+    private String Name;
+
+    public User(String name, String password) {
+        Name = name;
+        Password = password;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    private String Password;
+
+}
