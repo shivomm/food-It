1e0c427e859a912ce0af0610b6e469fee5b2727a
Shivom@1 <pandeyshivom43@gmail.com>
2020-01-22 09:50:59 -0800

    foodview Holder class menu holder class is added

--- app/src/main/java/com/shivom/foodit/foodlist.java
+++ app/src/main/java/com/shivom/foodit/foodlist.java
@@ -1,14 +1,24 @@
 package com.shivom.foodit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class foodlist extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    FirebaseDatabase database;
+    DatabaseReference foodList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodlist);
+        database=FirebaseDatabase;
+
     }
 }
